d6e4e7554579191aa00af2773a36bf9e9c91c848
rubenalarcongalvez <rubenalarcongalvez@gmail.com>
2024-07-05 16:20:35 +0200

    hotfix

--- src/app/views/fieldset/fieldset.component.ts
+++ src/app/views/fieldset/fieldset.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PrimeNgModule } from '../../shared/style/prime-ng/prime-ng.module';
 
 @Component({
   selector: 'app-fieldset',
   standalone: true,
-  imports: [],
+  imports: [PrimeNgModule],
   templateUrl: './fieldset.component.html',
   styleUrl: './fieldset.component.scss'
 })
